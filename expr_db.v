09b3ce567b9e13a124ab51b8ce7b870cf73edf9b
George Stelle <stelleg@gmail.com>
2015-07-13 07:24:37 -0600

    Added value and fixed db expr.

--- expr_db.v
+++ expr_db.v
@@ -5,6 +5,16 @@ Inductive expr (n : nat) : Type :=
   | lam : expr (S n) -> expr n
   | app : expr n -> expr n -> expr n.
 
+Definition value {n} (t : expr n) : Prop := match t with
+  | lam _ => True
+  | _ => False
+  end.
+
+Notation " :λ N " := (forall n, lam n N) (at level 20).
+Notation " M @ N " := (forall n, app n M N) (at level 60). 
+Coercion var : Fin.t >-> expr.
+
 Example term_1 : expr 0 := lam 0 (var 1 Fin.F1).
 Example term_2 : expr 0 := app 0 term_1 term_1.
+
 
