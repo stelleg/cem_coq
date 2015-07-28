5cf7ef029e5352edebf046404d0e32143ac49e41
George Stelle <stelleg@gmail.com>
2015-07-28 07:12:35 -0600

    Added bisim, started cem_cbn_bisim, and initial assembly

---
+++ assembly.v
@@ -0,0 +1,52 @@
+Inductive HeapPtr := | h_ptr : nat -> HeapPtr.
+Inductive StackPtr := | s_ptr : nat -> StackPtr.
+Inductive InstrPtr := | i_ptr : nat -> InstrPtr.
+
+Inductive Ptr := 
+  | ph : HeapPtr -> Ptr
+  | ps : StackPtr -> Ptr
+  | pl : InstrPtr -> Ptr.
+
+Inductive Word : Type := 
+  | wptr : Ptr -> Word
+  | wnat : nat -> Word.
+
+Inductive Reg := 
+  | IP
+  | SP
+  | EP 
+  | FR
+  | R1.
+
+Inductive L : Type :=
+  | lreg : Reg -> L
+  | lhptr : HeapPtr -> L
+  | lsptr : StackPtr -> L.
+
+Inductive R : Type :=
+  | rreg : Reg -> R
+  | rloc : Reg -> R
+  | rword : Word -> R.
+
+Inductive Op : Type :=
+  | add : R -> R -> Op.
+
+Inductive Instr : Type :=
+  | iset : L -> R -> Instr
+  | iop : L -> Op -> Instr.
+
+Inductive Jump : Type :=
+  | goto : InstrPtr -> Jump
+  | jzero : R -> InstrPtr -> InstrPtr -> Jump.
+
+Definition Heap := HeapPtr -> Word.
+Definition Stack := StackPtr -> Word.
+Definition Program := InstrPtr -> Instr. 
+Definition Registers := Reg -> Word.
+
+Inductive State := 
+  | st : Registers -> Program -> Stack -> Heap -> State.
+
+Inductive Step : State -> State -> Prop :=
+  | step : forall r p s h, r IP = iop l add,  st r p s h -> st r' p s h -> State.
+
---
+++ cem_cbn_bisim.v
@@ -0,0 +1,10 @@
+Require Import bisim.
+Require cem cbn.
+
+Inductive state_rel : relation cem.configuration cbn.configuration := 
+  | empty : 
+
+Theorem cem_cbn_bisim : bisim
+  v b
+
+
