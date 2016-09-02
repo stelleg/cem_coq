14849c2da1b1b4ffcf1bafd2d75d17b4f74f3a1e
George Stelle <gwstell@sandia.gov>
2016-09-02 12:28:09 -0600

    Initial commit with paper

--- compiler.v
+++ compiler.v
@@ -1,7 +1,7 @@
 Require expr cbn cem cesm im db assembly.
 Require cbn_cem cem_cesm.
 Require Import Unicode.Utf8.
-Require Import util expr_db db_assembly.
+Require Import util expr_db db_assembly relations.
 
 (* This file has the high level structure of the compiler and the corresponding
 main theorems of correctness *)
@@ -11,6 +11,15 @@ Definition compile (t : expr.tm) : option assembly.Program := match db t nil wit
   | None => None 
   end.
 
+Notation " t `⇝c` p " := (compile t = Some p) (at level 30).
+
+Lemma bisim_cem_st : cem.step ~ assembly.step.
+Admitted.
+
+Notation " t ≃ p " := (bisim_cem_st t p) (at level 30). 
+
+(*
+Lemma compile_bisim : ∀ t p, I
 (* Definition compile (t: expr.tm) : expr.closed t → { mc | trans_clos prog p }.
 *)
 
